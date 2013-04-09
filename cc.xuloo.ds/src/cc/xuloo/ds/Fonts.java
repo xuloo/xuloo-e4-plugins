58ab12d5e3405cabf6c3524caa20b23b8f6fc72b
Trevor Burton <Trevor Burton@192.168.2.128>
2013-04-09 13:22:35 +0100

    added some default font-ery

--- cc.xuloo.ds/src/cc/xuloo/ds/Fonts.java
+++ cc.xuloo.ds/src/cc/xuloo/ds/Fonts.java
@@ -1,9 +1,24 @@
 package cc.xuloo.ds;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.widgets.Display;
 
 public class Fonts {
 	
 	public static final String DEFAULT_FONT_NAME = "sans-serif";
+	
+	public static final Font bold14;
+	public static final Font bold12;
+	public static final Font normal;
+	public static final Font small;
+
+	static {
+		bold14 = new Font(Display.getDefault(), DEFAULT_FONT_NAME, 12, SWT.BOLD);
+		bold12 = new Font(Display.getDefault(), DEFAULT_FONT_NAME, 11, SWT.BOLD);
+		normal = new Font(Display.getDefault(), DEFAULT_FONT_NAME, 12, SWT.NONE);
+		small = new Font(Display.getDefault(), DEFAULT_FONT_NAME, 10, SWT.NONE);
+	}
 
 //	public static final String HELVETICA_NEUE_LT_PRO_LIGHT = "HelveticaNeueLTPro-Lt.otf";
 //
